c96192e889caaeecc7904a3ea33af5f611ffb1cf
Lukas Nickel <lukasnickel96@googlemail.com>
2018-11-15 10:49:25 +0100

    Reworked config behaviour
    - moved configs to config folder
    - fallback to sample_config removed
    - exception when config file is missing entries

--- cta_preprocessing/parameters.py
+++ cta_preprocessing/parameters.py
@@ -20,13 +20,24 @@ class PREPConfig(object):
         'output_suffix',
     )
     def __init__(self, config_file):
-        #sample_file = pathlib.Path(sys.path[0] + '/sample_config.yaml')
-        sample_file=('/home/lnickel/Documents/cta_prep_module/cta_preprocessing/cta_preprocessing/sample_config.yaml')
-        with open(config_file) as config, open(sample_file) as sample:
+        with open(config_file) as config:
             config = yaml.load(config)
-            sample = yaml.load(sample)
         for x in self.__slots__:
             if config.get(x) is not None:
                 self.__setattr__(x, config.get(x))
             else:
-                self.__setattr__(x, sample.get(x))
+                raise MissingConfigEntry('PREPConfig.__init__()',
+                                         'Missing entry in Config file for: '
+                                         + x)
+
+
+class MissingConfigEntry(Exception):
+    """Exception raised for errors in the input.
+
+    Attributes:
+        expression -- input expression in which the error occurred
+        message -- explanation of the error
+    """
+    def __init__(self, expression, message):
+        self.expression = expression
+        self.message =  message
