22cfc06dbeda5ca69ac5a7e2cc342ca1b404ddc3
Lukas Nickel <lukasnickel96@googlemail.com>
2019-01-31 13:51:51 +0100

    this will probably not be used anytime soon

--- cta_preprocessing/parameters.py
+++ cta_preprocessing/parameters.py
@@ -38,24 +38,6 @@ class PREPConfig(object):
                                          + x)
 
 
-class ImageCleaningConfig(object):
-    __slots__ = ('cleaning_level',
-                 'min_num_islands',
-                 'min_width',
-                 'min_length',
-                 )
-
-    def __init__(self, config_file):
-        with open(config_file) as config:
-            config = yaml.load(config)
-        for x in self.__slots__:
-            if config.get(x) is not None:
-                self.__setattr__(x, config.get(x))
-            else:
-                raise MissingConfigEntry('PREPConfig.__init__()',
-                                         'Missing entry in Config file for: '
-                                         + x)
-
 
 class MissingConfigEntry(Exception):
     """Exception raised for missing entries in the given config file.
